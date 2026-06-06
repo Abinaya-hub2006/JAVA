333734d0cd62a5f0d389848b02f072fbb12d248a
Abinaya <abinayak829@gmail.com>
2026-06-06 20:51:44 +0530

    Add files via upload

--- Basics/Main.java
+++ Basics/Main.java
@@ -8,5 +8,19 @@ public class  Main{
 
   int ans=0;
   System.out.println("Hello world!");
+
+  Scanner sc=new Scanner(System.in);
+  System.out.print("What is your name:");
+  String name=sc.next();
+
+  System.out.print("Age:");
+  int Age=sc.nextInt();
+
+  System.out.print("CGPA:");
+  double cgpa=sc.nextDouble();
+
+  System.out.println("Name= "+ name);
+  System.out.println("Age= "+ Age);
+  System.out.println("CGPA= "+ cgpa);
   }
 }

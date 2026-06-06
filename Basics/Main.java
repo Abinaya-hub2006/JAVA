9346e356a9f1c58340ebf15658b2a9052416c5d9
Abinaya <abinayak829@gmail.com>
2026-06-06 20:23:50 +0530

    Add files via upload

---
+++ Basics/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class  Main{
+  public static void main(String[] args){
+    //This is single line comment
+    /* multiline 
+    comment*/
+
+  int ans=0;
+  System.out.println("Hello world!");
+  }
+}

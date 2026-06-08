1e1c0826b8dbbc4c8c18f035c66f3185726b5d05
Abinaya <abinayak829@gmail.com>
2026-06-08 20:13:20 +0530

    Add files via upload

---
+++ Methods/Methods.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Methods {
+    static int add(int a,int b){
+      return a+b;
+    }
+    static int sub(int a,int b){
+      return a-b;
+    }
+    public static void main(String[] args) {
+      System.out.println(add(1,2));
+      System.out.println(sub(2,1));
+    }
+}

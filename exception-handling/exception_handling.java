86067b0efb5d312eb294db323b763742e3cd58f2
Abinaya <abinayak829@gmail.com>
2026-06-11 10:30:08 +0530

    Add files via upload

---
+++ exception-handling/exception_handling.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      //System.out.println("Hello, World!");
+      int arr[]={1,2,3};
+      try{
+        
+          System.out.println(arr[5]);
+      }catch(Exception e){
+          System.out.println(e);
+      }
+    }
+}
